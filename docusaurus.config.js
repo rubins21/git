5b4dfc7a42ca2fe23ca360500c49ad5ff62eec2b
rubins21 <35021024+rubins21@users.noreply.github.com>
2023-01-18 19:12:54 +0545

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'FMADIO',
   tagline: 'Documentation',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docusaurus-fmadio.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
